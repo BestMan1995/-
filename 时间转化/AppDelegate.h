22db085e9ff9c10f564c10e4f692c87f9e420f01
Apple <apple@wangbindeMacBook-Pro.local>
2017-03-24 11:24:01 +0800

    Initial Commit

---
+++ 时间转化/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  时间转化
+//
+//  Created by Apple on 2017/3/24.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
